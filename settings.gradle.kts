d31184094e67b0649326a004898fde1646946cce
kov3y <ginzow.jett@gmail.com>
2026-06-05 17:17:30 -0400

    chore: configure dependency resolution management and add Spigot Maven repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
 rootProject.name = "actionbar-api"
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+  repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+
+  repositories {
+    mavenCentral()
+
+    maven("https://hub.spigotmc.org/nexus/content/repositories/public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+  }
+}
